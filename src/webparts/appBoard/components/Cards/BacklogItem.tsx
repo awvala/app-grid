39c11f8e3f1722feb553051193ac1d71a92d4ed0
Andrew Vala <38055092+awvala@users.noreply.github.com>
2019-03-12 13:41:38 -0500

    Removed generic Modal component and cleaned up BacklogItem and BacklogModal compnents

--- src/webparts/appBoard/components/Cards/BacklogItem.tsx
+++ src/webparts/appBoard/components/Cards/BacklogItem.tsx
@@ -2,10 +2,7 @@ import * as React from 'react';
 import styles from '../AppBoard.module.scss';
 import Moment from 'react-moment';
 import ReactHtmlParser from 'react-html-parser';
-import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
-import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
-import { ModalBasicExample } from './BacklogModal';
-// mport Modal from 'react-awesome-modal';
+import { BacklogModal } from './BacklogModal';
 
 export const BacklogItem = props => {
     
@@ -23,79 +20,11 @@ export const BacklogItem = props => {
                 }
             </div>
             <p className={`ms-fontSize-sPlus ${styles.cardDescription}`}>{ReactHtmlParser(html)}</p>
-            <PrimaryButton
-                className={styles.cardButton}
-                data-automation-id={props.id}
-                secondaryText="See additional app information"
-                text="Read More"
-                onClick={props._ShowModal}
-                allowDisabledFocus={true}
-            />
-            <ModalBasicExample
+            <BacklogModal
                 Title = {props.title}
                 id = {props.id}
                 html = {html}
             />
-            {/* <Modal
-                visible={this.props.visible}
-                width="400"
-                height="300"
-                effect="fadeInUp"
-                onClickAway={() => this.closeModal()}
-            >
-                <div className={styles.modalHeader}>
-                    <span id={this.props.id}>{this.props.Title}</span>
-                </div>
-                <div className={styles.modalBody}>
-                    <DefaultButton onClick={this.closeModal} text="Close" />
-                    <br></br>
-                    <p>{ReactHtmlParser(this.props.html)}</p>
-                </div>
-            </Modal> */}
         </div>
     );
 };
-
-// export interface IModalBasicExampleState {
-//     showModal: boolean;
-//   }
-  
-//   export class ModalBasicExample extends React.Component<{}, IModalBasicExampleState> {
-//     public state: IModalBasicExampleState = {
-//       showModal: false
-//     };
-
-//     public openModal() {
-//         this.setState({
-//             visible: true
-//         });
-//     }
-
-//     public closeModal() {
-//         this.setState({
-//             visible: false
-//         });
-//     }
-
-//     public render() {
-//         return (
-//             <Modal
-//                 visible={this.props.visible}
-//                 width="400"
-//                 height="300"
-//                 effect="fadeInUp"
-//                 onClickAway={() => this.closeModal()}
-//             >
-//                 <div className={styles.modalHeader}>
-//                     <span id={this.props.id}>{this.props.Title}</span>
-//                 </div>
-//                 <div className={styles.modalBody}>
-//                     <DefaultButton onClick={this.closeModal} text="Close" />
-//                     <br></br>
-//                     <p>{ReactHtmlParser(this.props.html)}</p>
-//                 </div>
-//             </Modal>
-//         );
-//     }
-// }
-
--- src/webparts/appBoard/components/Cards/BacklogModal.tsx
+++ src/webparts/appBoard/components/Cards/BacklogModal.tsx
@@ -2,54 +2,33 @@ import * as React from 'react';
 import styles from '../AppBoard.module.scss';
 import Moment from 'react-moment';
 import ReactHtmlParser from 'react-html-parser';
+import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
 import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
-import { getId } from 'office-ui-fabric-react/lib/Utilities';
 import { Modal } from 'office-ui-fabric-react/lib/Modal';
 
-// export const BacklogModal = props => {
-
-//     return (
-
-//         <div>
-//             <Modal
-//             modalKey=
-//             modalState={ui.Modal}
-//             >
-//                 <div className={styles.modalHeader}>
-//                     <span id={props.id}>{props.title}</span>
-//                 </div>
-//                 <div className={styles.modalBody}>
-//                     <DefaultButton onClick={this._CloseModal} text="Close" />
-//                     <br></br>
-//                     <p>{ReactHtmlParser(props.html)}</p>
-//                 </div>
-//             </Modal>
-//         </div>
-//     );
-// };
-
-
-export interface IModalBasicExampleState {
+export interface IBacklogModalState {
     showModal: boolean;
   }
   
-  export class ModalBasicExample extends React.Component<{Title: string, html: any, id: string}, IModalBasicExampleState> {
-    public state: IModalBasicExampleState = {
+  export class BacklogModal extends React.Component<{Title: string, html: any, id: string}, IBacklogModalState> {
+    public state: IBacklogModalState = {
       showModal: false
     };
-
-// Use getId() to ensure that the IDs are unique on the page.
-  // (It's also okay to use plain strings without getId() and manually ensure uniqueness.)
-  private _titleId: string = getId('title');
-  private _subtitleId: string = getId('subText');
 
   public render(): JSX.Element {
     return (
       <div>
-        <DefaultButton secondaryText="Opens the Sample Modal" onClick={this._showModal} text="Open Modal" />
+        <PrimaryButton
+            className={styles.cardButton}
+            data-automation-id={this.props.id}
+            secondaryText="See additional app information"
+            text="Read More"
+            onClick={this._showModal}
+            allowDisabledFocus={true}
+            />
         <Modal
           titleAriaId={this.props.id}
-          subtitleAriaId={this._subtitleId}
+
           isOpen={this.state.showModal}
           onDismiss={this._closeModal}
           isBlocking={false}
@@ -58,7 +37,7 @@ export interface IModalBasicExampleState {
           <header className="ms-modalExample-header">
             <h2>{this.props.Title}</h2>
           </header>
-          <div id={this._subtitleId} className="ms-modalExample-body">
+          <div className="ms-modalExample-body">
             <p>{ReactHtmlParser(this.props.html)}</p>
           </div>
           <footer className="footerPlaceholder">
